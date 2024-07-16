6287dec3287ff1d01842f5f9c7f67f876f7c7d24
samwanyua <samexample8@gmail.com>
2024-07-16 22:38:13 +0300

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 // src/App.js
-import './App.css';
+// import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home movies={movies}/>}></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
-            <Route path="*" element = {<NotFound/>}></Route>
-        </Route>
-      </Routes>
+      <h4>Welcome Sam</h4>
     </div>
   );
 }
