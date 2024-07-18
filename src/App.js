d5c1a504d9a145c18d0c348e6d2ff4d508e75d28
samwanyua <samexample8@gmail.com>
2024-07-18 23:43:44 +0300

    implement useState

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { useState } from "react";
 
 
 function App() {
-  const counter = useState(0) // 0 is the initial value 
-  console.log(counter)
+  const [counter, setCounter] = useState(0); // 0 is the initial value 
+  console.log(counter);
 
+  const [role, setRole] = useState("Developer");
+ 
 
-  let role = "Developer";
+  // let role = "Developer";
 
   const changeColor = () => {
-    role = "Architect"
+    // role = "Architect"
+    setRole("Architect");
     // console.log(role)
   }
 
