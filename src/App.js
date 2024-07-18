114d39bf4c8307af0983cd2fe7e94d6fa4183ece
samwanyua <samexample8@gmail.com>
2024-07-18 23:54:47 +0300

    implement useState

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0); // 0 is the initial value 
-  console.log(counter);
+  // console.log(counter);
 
   const [role, setRole] = useState("Developer"); // 'Developer' is the initial value
  
@@ -18,11 +18,28 @@ function App() {
     // console.log(role)
   }
 
+  // const [brand, setBrand] = useState('BMW');
+  // const [model, setModel] = useState('X6');
+  // const [year, setYear] = useState('2020');
+  // const [color, setColor] = useState('Silver');
+
+  const [car, setCar] = useState({
+    brand: "BMW",
+    model: "X6",
+    year: "2020",
+    color: "Silver"
+  });
+
+
+
 
   return (
     <>
       <h1>Hi, I am a Software {role}</h1>
       <button onClick={changeColor}>Hire me!</button>
+
+      <h1>My {car.brand} </h1>
+      <h3>It's a {car.color} {car.model} {car.year},straight from Germany</h3>
     </>
    
   );
