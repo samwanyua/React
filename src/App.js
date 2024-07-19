be4e60d60ea8c57a99f85fb0ce1efdc731ddcfa8
samwanyua <samexample8@gmail.com>
2024-07-19 14:04:19 +0300

    Implement useEffect with dependency

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
 
   const [cout, setCout] = useState(0);
 
-  // useEffect without any dependencies
+  // useEffect without any dependencies - it executes callback function continously
   // useEffect(() => {
   //   setTimeout(()=>{
   //     setCout((cout) => cout+1)
@@ -69,11 +69,19 @@ function App() {
 
 
   // execute the callback function only once when the component gets loaded
+  // useEffect(() => {
+  //   setTimeout(()=>{
+  //     setCout((cout) => cout+1)
+  //   },2000)
+  // },[])
+
+
+  // whenever count changes it will again execute the callback function
   useEffect(() => {
     setTimeout(()=>{
       setCout((cout) => cout+1)
     },2000)
-  },[])
+  },[cout])    
 
 
 
